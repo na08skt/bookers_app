b6a63c9da02c3e6770b2222a20f6455bb67ead27
EC2 Default User <ec2-user@ip-172-31-81-128.ec2.internal>
2022-05-09 08:16:50 +0000

    [InitialCommit]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book.id)
+      flash[:create] = "Book was successfully created."
     else
       @books = Book.all
       render :index
@@ -27,19 +28,22 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_params
-    flash[:edit] = "Book was successfully updated."
+    if @book.update(book_params)
+      redirect_to book_path
+      flash[:edit] = "Book was successfully updated."
+    else
+      redirect_to edit_book_path
+      flash[:error] = "Book wasn't successfully updated.error"
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    if
-    book.destroy
-    redirect_to books_path
-    flash[:notice] = "Book was successfully destroyed."
+    if  book.destroy
+      redirect_to books_path
+      flash[:notice] = "Book was successfully destroyed."
     else
-    flash[:alert] = "Book wasn't successefully destroyed."
+      flash[:alert] = "Book wasn't successefully destroyed."
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # top pageのルーティング
-  get '/' => 'homes#top'
+  root :to => 'homes#top'
   # top page 表示　get
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
